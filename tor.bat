c19ac621c3c17ce5dda030fa3866a0c827426a8b
Hamid Rohani <hamid914@users.noreply.github.com>
2019-07-01 15:54:15 +0430

    add scripts to run tor without its bundle

---
+++ tor.bat
@@ -0,0 +1,2 @@
+cd ..\Browser
+TorBrowser\Tor\tor.exe --defaults-torrc TorBrowser\Data\Tor\torrc-defaults -f TorBrowser\Data\Tor\torrc +__SocksPort 127.0.0.1:9150 | more
---
+++ tor.sh
@@ -0,0 +1,5 @@
+cd ../Browser
+TorBrowser/Tor/tor.exe \
+--defaults-torrc TorBrowser/Data/Tor/torrc-defaults \
+-f TorBrowser/Data/Tor/torrc \
++__SocksPort 127.0.0.1:9150
